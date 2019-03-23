068e2b73db67c09fb2bc6771ffa6f5bdc67a2a88
John Langhauser <john.langhauser@gmail.com>
2019-03-23 13:02:57 -0400

    adding in styling changes for student html

--- checkin/handlers/students.py
+++ checkin/handlers/students.py
@@ -41,10 +41,18 @@ class StudentHandler(BaseHandler):
             for indx, or_rule in enumerate(or_rules):
                 if visitor and visitor.or_progress and len(visitor.or_progress) > indx:
                     or_rule.progress = visitor.or_progress[indx]
+                    or_rule.num_progress = int(visitor.or_progress[indx] * or_rule.num_checkins / 100)
+                else:
+                    or_rule.progress = 0
+                    or_rule.num_progress = 0
 
             for indx, and_rule in enumerate(and_rules):
                 if visitor and visitor.and_progress and len(visitor.and_progress) > indx:
                     and_rule.progress = visitor.and_progress[indx]
+                    and_rule.num_progress = int(visitor.and_progress[indx] * and_rule.num_checkins / 100)
+                else:
+                    and_rule.progress = 0
+                    or_rule.num_progress = 0
 
         params['or_rules'] = or_rules
         params['and_rules'] = and_rules
